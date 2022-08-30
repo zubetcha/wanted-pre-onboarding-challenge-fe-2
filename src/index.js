9a7893f57a2202d009c489c4c36a9d241f062bf7
zubetcha <zuhye5@gmail.com>
2022-08-31 00:18:42 +0900

    :memo: generate Todo class

--- src/index.js
+++ src/index.js
@@ -1,51 +1,51 @@
 /**
- * Represents a book.
- * @constructor
- * @param {string} title - The title of the book.
- * @param {string} author - The author of the book.
+ * Creates a new Todo.
+ * @class
  */
-function Book(title, author) {}
+function Todo() {}
 
 /**
- * Both of these will link to the bar function.
- * @see {@link bar}
- * @see bar
+ * 할 일을 추가할 수 있다.
+ * 내용없이 추가할 수 없다.
+ * @param {Object} todo - todo 객체
+ * @param {number} todo.id - required
+ * @param {string} todo.content - required
+ * @param {boolean} todo.isDone - required
+ * @param {string} todo.category - required
+ * @param {string[]} [todo.tags] - optional
+ * @return {Obejct} - 생성된 Todo 객체
  */
-function foo() {}
-
-// Use the inline {@link} tag to include a link within a free-form description.
-/**
- * @see {@link foo} for further information.
- * @see {@link http://github.com|GitHub}
- */
-function bar() {}
-
-/**
- * Generic dairy product.
- * @constructor
- */
-function DairyProduct() {}
+Todo.prototype.create = ({ id, content, isDone, category, tags }) => {};
 
 /**
- * Check whether the dairy product is solid at room temperature.
- * @abstract
- * @return {boolean}
+ * ID를 기반으로 특정 할 일을 조회할 수 있다.
+ * 모든 할 일을 조회할 수 있다.
+ * @param {number} [id] - optional 조회하려고 하는 Todo의 id
+ * @return {(Object | Object[])}
  */
-DairyProduct.prototype.isSolid = function () {
-	throw new Error('must be implemented by subclass!');
-};
+Todo.prototype.read = (id) => {};
 
 /**
- * Cool, refreshing milk.
- * @constructor
- * @augments DairyProduct
+ * ID를 제외한 모든 속성을 수정할 수 있다.
+ * 특정 할 일의 특정 태그를 수정할 수 있다.
+ * @param {number} id - required 수정하려고 하는 Todo의 id
+ * @param {number} [index] - optional 수정하려고 하는 태그의 인덱스
  */
-function Milk() {}
+Todo.prototype.update = (id, index) => {};
 
 /**
- * Check whether milk is solid at room temperature.
- * @return {boolean} Always returns false.
+ * ID를 기반으로 특정 할 일을 삭제할 수 있다.
+ * 모든 할 일을 제거할 수 있다.
+ * @param {number} [id] - optional 삭제하려고 하는 Todo의 id
  */
-Milk.prototype.isSolid = function () {
-	return false;
-};
+Todo.prototype.delete = (id) => {};
+
+/**
+ * 특정 할 일의 특정 태그를 삭제할 수 있다.
+ * 특정 할 일의 모든 태그를 제거할 수 있다.
+ * @param {number} id - required 태그를 삭제하려고 하는 Todo의 id
+ * @param {number} [index] - optional 삭제하려고 하는 태그의 인덱스
+ */
+Todo.prototype.deleteTags = (id, index) => {};
+
+const todo = new Todo();
